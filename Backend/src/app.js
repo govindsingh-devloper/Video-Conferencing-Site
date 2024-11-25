257d6e4c64f1d2a2bbca99c477b41267bb2384cb
Govind <govind.2022@iic.ac.in>
2024-11-25 16:30:53 +0530

    Deploying Backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -40,14 +40,19 @@ app.use(cookieParser());
 app.use(express.urlencoded({limit:"40kb",extended:true}));
 app.use(
     cors({
-        origin:"http://localhost:3000",
-        credentials:true
+      origin: [
+        "http://localhost:3000",  // Local frontend
+        "https://videofrontend-77zt.onrender.com",  // Deployed frontend
+      ],
+      credentials: true,
     })
-)
+  );
 
 
 //Routes
 app.use("/api/v1/auth",UserRoutes)
+app.options('*', cors());  // This handles preflight OPTIONS requests
+
 
 
 //default route
